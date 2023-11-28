df205d54a129481b07d576b7e7b736a274d2a577
Yordy Almonte <yoalmonte.dev@outlook.com>
2023-11-28 01:43:13 -0400

    Project Config Ready

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center">
+        <h1 className="mb-4">
+          <span className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-300 to-blue-600">
+            Hello World
+          </span>
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
