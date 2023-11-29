a2ba9fbaaa8655dd6e0ed14ed55c4d117ca6b504
Yordy Almonte <yoalmonte.dev@outlook.com>
2023-11-29 02:21:34 -0400

    UseEffect in .tsx

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 
 type Props = {
   url: string;
@@ -6,14 +6,33 @@ type Props = {
 
 export const Image = ({ url }: Props): JSX.Element => {
   const node = useRef<HTMLImageElement>(null)
-  
+  const [src, setSrc] = useState("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=")
+
+  useEffect(()=> {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setSrc(url);
+        }
+      });
+    });
+
+    if (node.current) {
+      observer.observe(node.current)
+    }
+
+    return () => {
+      observer.disconnect()
+    }
+  }, [url]);
+
   return (
     <figure className="flex justify-center">
       <img
         ref={node} 
-        src={url} 
+        src={src} 
         alt=""
-        className="w-80 h-auto rounded-md"
+        className="w-80 h-auto rounded-md bg-gray-200"
       />
     </figure>
   )
